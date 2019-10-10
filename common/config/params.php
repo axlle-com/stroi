580dde9b0e2bb323d234d53f5bbbfc426d2e7dbf
WORKER <axlle@mail.ru>
2019-10-10 22:58:08 +0300

    add all01

--- common/config/params.php
+++ common/config/params.php
@@ -1,9 +1,4 @@
 <?php
 return [
-    'name' => 'ООО "Сруб-Строй"',
-    'adminEmail' => 'www@srub-stroi.ru',
-    'supportEmail' => 'www@srub-stroi.ru',
-    'siteEmail' => 'info@srub-stroi.ru',
-    'user.passwordResetTokenExpire' => 3600,
-    'photo' => '/images/backgrounds/product/photo.jpg',
+
 ];
--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,7 +1,4 @@
 <?php
 return [
-    'name' => 'ООО "Сруб-Строй"',
-    'adminEmail' => 'www@srub-stroi.ru',
-    'supportEmail' => 'www@srub-stroi.ru',
-    'siteEmail' => 'info@srub-stroi.ru',
+
 ];
